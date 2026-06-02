a6caa49d57506407744b2b49a17c1b84d38bb6de
YousifSultan <231142289+yousifsultan2006-web@users.noreply.github.com>
2026-06-01 21:32:07 -0700

    did some more improvements, revised and changed up design and context

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
 import express from "express";
+import { fileURLToPath } from "url";
+import path from "path";
+
+const app = express();
+const PORT = 3000;
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use(express.static(__dirname));
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.listen(PORT, () => {
+    console.log(`Resume running at http://localhost:${PORT}`);
+});
